f5f8c02f0ea703ae226cd7f4b0f26c40f9fdee47
Robin Aly <robin@aly.de>
2015-03-26 15:44:19 +0100

    Begin of limas part

--- link_components.py
+++ link_components.py
@@ -4,11 +4,12 @@
 
 import os
 import subprocess
+import shutil
 from scaffoldutils import utils
 
 import logging
 log = logging.getLogger(__name__)
-logging.basicConfig()
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
 
 
 def prepare_cpuvisor(base_path, component_paths):
@@ -35,10 +36,19 @@ def prepare_cpuvisor(base_path, component_paths):
     with change_cwd_tmp(os.path.join(component_paths['cpuvisor-srv', 'bin'])):
         subprocess.call(["cpuvisor_preproc", "--nodsetfeats"])
 
-
 def prepare_limas(base_path, component_paths):
-    pass
-
+    log.info('[cpuvisor] Preparing config...')
+    with utils.change_cwd_tmp(component_paths['limas']):
+      utils.copyReplace(open('set_env.sh.template'), open('set_env.sh', 'w'), [
+        ('<directory to limas>', component_paths['limas']),        
+      ])
+      
+      utils.copyReplace(open('conf/conf-template.py'), open('conf/conf.py', 'w'), [
+        ('<COLLECTION NAME>', component_paths['limas']),
+        ('<Path web-accessible images and paths are stored>', component_paths['limas']),
+        ('<Path where indices and other runtime information of this instance will be stored>', component_paths['limas']),
+        ('<CPUVISOR_PORT>', component_paths['limas']),
+      ])
 
 def prepare_axes_home(base_path, component_paths):
     pass
@@ -54,11 +64,11 @@ if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.realpath(__file__))
     component_paths = utils.load_component_paths(file_dir)
 
-    log.info('Preparing cpuvisor-srv component...')
-    prepare_cpuvisor(file_dir, component_paths)
+    # log.info('Preparing cpuvisor-srv component...')
+    # prepare_cpuvisor(file_dir, component_paths)
 
     log.info('Preparing limas component...')
     prepare_limas(file_dir, component_paths)
 
-    log.info('Preparing axes-home component...')
-    prepare_axes_home(file_dir, component_paths)
+    # log.info('Preparing axes-home component...')
+    # prepare_axes_home(file_dir, component_paths)
