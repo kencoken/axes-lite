1e963c1bf3a8d2ca56e444c3c42798a35ea6c3c8
Ken Chatfield <ken.chatfield@gmail.com>
2015-03-27 11:52:27 +0000

    Small style changes

--- get_components.py
+++ get_components.py
@@ -25,18 +25,20 @@ for (key, value) in paths.iteritems():
     if key not in component_opts['paths'] or component_opts['paths'][key] == default_val:
         component_opts['paths'][key] = value
 
-dataPaths = {
+data_paths = {
     'data': 'data/',
     'index': 'index/',
     'collection': 'collection/',
 }
-for (key, value) in dataPaths.iteritems():
+
+for (key, value) in data_paths.iteritems():
     if key not in component_opts['data'] or component_opts['data'][key] == default_val:
         component_opts['data'][key] = value
         if not os.path.isdir(value):
           os.mkdir(value)
 
 # write away changes
+
 with open(utils.COMPONENT_CFGS_FILE, 'w') as f:
     json.dump(component_opts, f,
               sort_keys=True,
@@ -49,15 +51,19 @@ with open(utils.COMPONENT_CFGS_FILE, 'w') as f:
 # TODO: should download a specific tag
 
 if not os.path.isdir(component_opts['paths']['cpuvisor-srv']):
-    subprocess.call('git clone git@github.com:kencoken/cpuvisor-srv.git ' + component_opts['paths']['cpuvisor-srv'], shell=True)
+    subprocess.call(['git clone git@github.com:kencoken/cpuvisor-srv.git',
+                     component_opts['paths']['cpuvisor-srv']], shell=True)
     with utils.change_cwd('cpuvisor-srv'):
         subprocess.call('git checkout al-integration-prep', shell=True) # TODO: remove this custom branch
 
 if not os.path.isdir(component_opts['paths']['imsearch-tools']):
-    subprocess.call('git clone git@github.com:kencoken/imsearch-tools.git ' + component_opts['paths']['imsearch-tools'], shell=True)
+    subprocess.call(['git clone git@github.com:kencoken/imsearch-tools.git',
+                     component_opts['paths']['imsearch-tools']], shell=True)
 
 if not os.path.isdir(component_opts['paths']['limas']):
-    subprocess.call('hg clone ssh://hg@bitbucket.org/alyr/limas ' + component_opts['paths']['limas'], shell=True)
+    subprocess.call(['hg clone ssh://hg@bitbucket.org/alyr/limas',
+                     component_opts['paths']['limas']], shell=True)
 
 if not os.path.isdir(component_opts['paths']['axes-home']):
-    subprocess.call('git clone git@bitbucket.org:kevinmcguinness/axes-home.git ' + component_opts['paths']['axes-home'], shell=True)
+    subprocess.call(['git clone git@bitbucket.org:kevinmcguinness/axes-home.git',
+                     component_opts['paths']['axes-home']], shell=True)
