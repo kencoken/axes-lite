23bd6194533d326a53800361324bf7d21d309348
Ken Chatfield <ken.chatfield@gmail.com>
2015-04-15 21:55:58 +0100

    Added automatic loading of custom environment to cpuvisor-srv start.sh

--- templates/cpuvisor-srv/start.sh
+++ templates/cpuvisor-srv/start.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# if an individual environment setup exists, source it.
+if [ -f "setup_env.sh" ]; then
+  source ./setup_env.sh
+fi
+
 cd bin
 ./cpuvisor_service --config_path ../config.{name}.prototxt
